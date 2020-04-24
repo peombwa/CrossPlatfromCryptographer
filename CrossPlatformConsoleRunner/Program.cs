1aa075fd9232c9cf3d2dffb40ccb86f7f9de5440
Peter Ombwa <peombwa@microsoft.com>
2020-04-24 15:12:48 -0700

    Initial commit

---
+++ CrossPlatformConsoleRunner/Program.cs
@@ -0,0 +1,34 @@
+﻿using CrossPlatformCryptographer;
+using System;
+
+namespace CrossPlatformConsoleRunner
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string token = "NotSecureToken";
+            Console.WriteLine($"Plain content: {token}");
+            try
+            {
+                Console.WriteLine("Encypting...");
+                Cryptographer.Encrypt(token).GetAwaiter().GetResult();
+                Console.WriteLine("Encrypted");
+                Console.WriteLine("Decypting...");
+                string decryptedToken = Cryptographer.Decrypt(token).GetAwaiter().GetResult();
+                Console.WriteLine($"Decypted content: {decryptedToken}");
+                Console.WriteLine("Removing key...");
+                Cryptographer.Remove();
+                Console.WriteLine("Removed");
+                Console.WriteLine("Decypting...");
+                decryptedToken = Cryptographer.Decrypt(token).GetAwaiter().GetResult();
+                Console.WriteLine($"Decypted content: {decryptedToken}");
+            }
+            catch (Exception ex)
+            {
+
+                Console.WriteLine($"Device doesn't support secure storage. {ex.Message}");
+            }
+        }
+    }
+}
---
+++ CrossPlatformCryptographer/Cryptographer.cs
@@ -0,0 +1,23 @@
+﻿using System.Threading.Tasks;
+using Xamarin.Essentials;
+
+namespace CrossPlatformCryptographer
+{
+    public static class Cryptographer
+    {
+        public static async Task Encrypt(string plainText)
+        {
+            await SecureStorage.SetAsync("msal_oauth_token", plainText).ConfigureAwait(false);
+        }
+
+        public static async Task<string> Decrypt(string cypherText)
+        {
+            return await SecureStorage.GetAsync("msal_oauth_token").ConfigureAwait(false);
+        }
+
+        public static void Remove()
+        {
+            SecureStorage.Remove("msal_oauth_token");
+        }
+    }
+}
